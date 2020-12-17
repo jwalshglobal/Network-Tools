28850fb7699fb14495f51eee43667abe10370401
jwalshglobal <jwalshglobal@gmail.com>
2020-12-17 06:53:09 -0800

    Notes on Week 2 and updating formatting of notes

---
+++ pynet-learning-python/Week 2/test-lists.py
@@ -0,0 +1,27 @@
+Lists - Sequential Data
+
+# Initalize the variable
+my_list = []
+
+# Mix of data types
+my_list = ['whatever', 22, 4.0]
+
+#Print a slice of the list
+print(my_list[0])
+
+len(my_list)
+
+# Add to the list
+my_list.append('something')
+
+# Extend the list
+my_list.extend(['20','30'])
+
+#Pop End
+my_list.pop()
+
+#Pop Beginning
+my_list.pop(0)
+
+#Remove beginning of list
+del my_list[0]
