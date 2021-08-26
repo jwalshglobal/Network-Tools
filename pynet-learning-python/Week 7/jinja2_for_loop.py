6c17b71d739635fd79483d50ede3ee8f0739f7df
jwalshglobal <jwalshglobal@gmail.com>
2021-08-26 15:33:08 -0700

    Jinja for loops and conditionals

--- pynet-learning-python/Week 7/jinja2_for_loop.py
+++ pynet-learning-python/Week 7/jinja2_for_loop.py
@@ -3,15 +3,14 @@
 from __future__ import print_function, unicode_literals
 import jinja2
 
+advertised_routes = ["10.10.200.0/24", "10.10.201.0/24", "10.10.202.0/24"]
 bgp_vars = {
     "peer1_ip": "10.255.255.2",
     "peer1_as": "20",
-    "advertised_route1": "10.10.200.0/24",
-    "advertised_route2": "10.10.201.0/24",
-    "advertised_route3": "10.10.202.0/24",
+    "advertised_routes": advertised_routes
 }
 
-template_file = 'nxos_bgp.txt'
+template_file = 'nxos_bgp2.txt'
 with open(template_file) as f:
     bgp_template = f.read()
 
--- pynet-learning-python/Week 7/jinja2_for_loop_conditional.py
+++ pynet-learning-python/Week 7/jinja2_for_loop_conditional.py
@@ -3,15 +3,15 @@
 from __future__ import print_function, unicode_literals
 import jinja2
 
+advertised_routes = ["10.10.200.0/24", "10.10.201.0/24", "10.10.202.0/24"]
 bgp_vars = {
     "peer1_ip": "10.255.255.2",
     "peer1_as": "20",
-    "advertised_route1": "10.10.200.0/24",
-    "advertised_route2": "10.10.201.0/24",
-    "advertised_route3": "10.10.202.0/24",
+    "peer1_ipv6": True,
+    "advertised_routes": advertised_routes
 }
 
-template_file = 'nxos_bgp.txt'
+template_file = 'nxos_bgp3.txt'
 with open(template_file) as f:
     bgp_template = f.read()
 
