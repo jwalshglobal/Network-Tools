05fd95243dbaba6a3729c79ab9d10f8973683db6
John Walsh <47428915+jwalshglobal@users.noreply.github.com>
2021-09-15 15:43:56 -0700

    Python various essentials (#15)

---
+++ pynet-learning-python/Week 8/libraries_fun.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# Package or Module ?
+# Package has a directory, while module is a single file
+
+import re
+
+"""
+PyDev console: starting.
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 16:52:21) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+>>> import re
+>>> re.__file__
+'/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/re.py'
+"""
+
+help(re.search)
---
+++ pynet-learning-python/Week 8/virtual_env.py
@@ -0,0 +1,17 @@
+# Create a sandbox to manage dependencies
+# Decouple system python (usr/bin) with running verion of python
+# Manage different dependencies
+
+# Lots of different ways to use virtual env
+
+# mkdir VENV
+# cd VENV
+# virtualenv -p /usr/bin/python36 test_venv
+
+# Activate Virtual Environment
+# source test_venv/bin/activate
+# which python
+# pip list
+
+# Exit out of virtual Env
+# deactivate
